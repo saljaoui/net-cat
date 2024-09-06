1bcd78a01ca77ccf4d5e300faa52f18e371e3df1
saljaoui <soufian2022ff@gmail.com>
2024-09-06 22:54:43 +0100

    s

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	ListenAddr string
+	ln         net.Listener
+	quitch     chan struct{}
+}
+
+func NewServer(ListenAddr string) *Server {
+	return &Server{
+		ListenAddr: ListenAddr,
+		quitch:     make(chan struct{}),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	s.ln = ln
+
+	go s.acceptLoop()
+
+	<-s.quitch
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	
+	for {
+		con, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			continue
+		}
+		go s.readLoop(con)
+	}
+
+}
+
+func (s *Server) readLoop(con net.Conn) {
+	defer con.Close()
+	for {
+
+	}
+}
+
+func main() {
+	server := NewServer(":3000")
+	server.Start()
+}
